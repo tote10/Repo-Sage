0a7896609d0610666a1fe62ca201883c85008ec4
tote10 <motialemu9@gmail.com>
2026-07-07 23:41:40 +0300

    little script

---
+++ hello.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq()
+
+try:
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role":"system", "content": "you tell them about ethiopain orthodox church very detailed"},{"role":"user", "content": "hey how is your day."}
+
+        ],)
+    print(response.choices[0].message.content)
+    
+except Exception as e:
+    print(f"Error: {e}")
